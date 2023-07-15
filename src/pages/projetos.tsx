20c704f6b22262350728d3a7c9b1103132b031e1
Guterresthomas <guterresmikael@gmail.com>
2023-07-15 00:14:44 -0300

    aa

---
+++ src/pages/projetos.tsx
@@ -0,0 +1,14 @@
+import NavMenu from "@/components/navMenu";
+import React from "react";
+import '../app/globals.css'
+
+const Projetos = () => {
+    return (
+        <div className='bg-zinc-900 h-full md:h-screen'>
+            <NavMenu />
+            
+        </div>
+    )
+}
+
+export default Projetos
